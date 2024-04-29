46f9bcb90caac505beea4f66e46df15b33dcb6cd
Dmytro Kozyr <59289925+kozyr-dv@users.noreply.github.com>
2024-04-29 13:55:04 +0000

    Update Files

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "google" {
 }
 
 module "gke_cluster" {
-  source         = "github.com/<ВАШ-РЕПОЗИТОРІЙ>/tf-google-gke-cluster"
+  source         = "github.com/kozyr-dv/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = var.GKE_NUM_NODES
