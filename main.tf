5e89861a99be1021633cc6f936111fc7a2b3f33b
Dmytro Kozyr <59289925+kozyr-dv@users.noreply.github.com>
2024-04-29 13:42:13 +0000

    Update Files

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.5"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "your-bucket-name"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  credentials = file("<YOUR-CREDENTIALS-FILE>.json")
+  project     = var.GOOGLE_PROJECT
+  region      = var.GOOGLE_REGION
+}
+
+module "gke_cluster" {
+  source         = "github.com/<ВАШ-РЕПОЗИТОРІЙ>/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = var.GKE_NUM_NODES
+}
